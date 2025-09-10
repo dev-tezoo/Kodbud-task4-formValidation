fc1aa69507ca4eca60bf94e931410f6be7a0f1ed
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-10 21:28:14 +0530

    name validations changes

--- script.js
+++ script.js
@@ -19,5 +19,26 @@ function validateName() {
         nameError.innerHTML = 'Name is required';
         return false;
     }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '';
+    return true;
+}
+function validateEmail() {
+    let email = document.getElementById('email').value
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '';
     return true;
 }
