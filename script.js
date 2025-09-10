01b28a5c2fd12051593a444dcd4f23051c2a9448
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-10 21:39:54 +0530

    email validated

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 const submitBtn = document.getElementById('submitBtn')
 const nameError = document.querySelector('.nameError')
+const emailError = document.querySelector('.emailError')
+const phoneError = document.querySelector('.phoneError')
 
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault()
     
-    if(validateName()){
+    if(validateName() && validateEmail()){
         alert('Form Submitted Successfully')
     }
 })
@@ -27,6 +29,8 @@ function validateName() {
     nameError.innerHTML = '';
     return true;
 }
+
+
 function validateEmail() {
     let email = document.getElementById('email').value
 
@@ -35,10 +39,27 @@ function validateEmail() {
         return false;
     }
 
-    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-        nameError.innerHTML = 'Write full name';
+    if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/)){
+        emailError.innerHTML = 'Write valid email';
         return false;
     }
-    nameError.innerHTML = '';
+    emailError.innerHTML = '';
+    return true;
+}
+
+
+function validateEmail() {
+    let email = document.getElementById('email').value
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if(!email.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/)){
+        emailError.innerHTML = 'Write valid email';
+        return false;
+    }
+    emailError.innerHTML = '';
     return true;
 }
